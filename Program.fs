c2fd3419bf471ae4920eb7fdb5765ed4148ffb89
Mark Thebridge <mark.thebridge@metaswitch.com>
2021-12-01 10:52:31 +0000

    Refactor and fmt

--- Program.fs
+++ Program.fs
@@ -1,29 +1,36 @@
-﻿
-module day1 =
-    let input = System.IO.File.ReadAllLines(@"day1input.txt")
-    let is_incr (x, y) =  x <  y
-    let incrs  =
-      input
-        |> Array.map int
+﻿module day1 =
+    let input =
+        System.IO.File.ReadAllLines(@"day1input.txt")
+
+    let is_incr (x, y) = x < y
+
+    let count_incr_steps list =
+        list
         |> Array.pairwise
         |> Array.filter is_incr
         |> Array.length
+
+    let incrs =
+        input |> Array.map int |> count_incr_steps
+
 
     printfn $"Part 1: {incrs}"
 
     let threewaysum (list: array<int>) =
-      let limit = list.Length - 2
-      let indices = [|for i in 0 .. list.Length -> i |]
-      indices
-        |> Seq.choose (fun (i) -> if i >= limit then Option.None else Option.Some(list[i] + list[i+1] + list[i+2]))
+        let limit = list.Length - 2
+
+        seq { 0 .. list.Length }
+        |> Seq.choose (fun (i) ->
+            if i >= limit then
+                Option.None
+            else
+                Option.Some(list[i] + list[i + 1] + list[i + 2]))
         |> Seq.toArray
 
     let newincrs =
-      input
+        input
         |> Array.map int
         |> threewaysum
-        |> Array.pairwise
-        |> Array.filter is_incr
-        |> Array.length
+        |> count_incr_steps
 
     printfn $"Part 2: {newincrs}"
