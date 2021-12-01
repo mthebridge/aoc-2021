c629bfbb0f8e8d06823e37c65951eeaa5138e463
Mark Thebridge <mark.thebridge@metaswitch.com>
2021-12-01 08:35:07 +0000

    Day 1 part1

---
+++ Program.fs
@@ -0,0 +1,13 @@
+﻿
+module day1 =
+
+    let input = System.IO.File.ReadAllLines(@"day1input.txt")
+    let is_incr (x, y) =  x <  y
+    let incrs  =
+      input
+        |> Array.map int
+        |> Array.pairwise
+        |> Array.filter is_incr
+        |> Array.length
+
+    printfn $"Part 1: {incrs}"
